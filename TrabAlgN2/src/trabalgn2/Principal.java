6e105fc735948074feaa34c8fb04a9e0f9c01479
KaioViana <kaiu.viana@hotmail.com>
2018-11-22 16:33:04 -0200

    Gerenciador de prédio residencial

--- TrabAlgN2/src/trabalgn2/Principal.java
+++ TrabAlgN2/src/trabalgn2/Principal.java
@@ -21,6 +21,10 @@ public class Principal {
             System.out.println("2.Quantidade de apartamentos ocupados em cada andar");
             System.out.println("3.O andar com maior e menor número de moradores");
             System.out.println("4.Quantidade de moradores com idade inferior a 30 anos");
+            System.out.println("5.A porcentagem de ocupação de cada andar");
+            System.out.println("6.A quantidade de moradores do sexo feminino no prédio");
+            System.out.println("7.Pesquisar morador pelo nome");
+            System.out.println("8.Sair");
             System.out.println("Opção: ");
             int opcao = Integer.parseInt(in.nextLine());
             // switcth-case
@@ -37,12 +41,28 @@ public class Principal {
                 case 4:
                     idadeInferior();
                     break;
+                case 5:
+                    porcentagem();
+                    break;
+                case 6:
+                    sexoFem();
+                    break;
+                case 7:
+                    pesquisarNome();
+                    break;
+                case 8:
+                    System.exit(0);
+                    System.out.println("\nFINALIZADO.");
+                default:
+                    System.out.println("ENTRADA INVÁLIDA!\n");
             }
         }
     }
     
     // Método adicionarMor(), adiciona novos moradores no prédio
     public static void adicionarMor(){
+        
+        System.out.println("======================= ADICIONAR =======================");
         
         // Pede o andar a ser inserido o novo morador
         System.out.println("\nInforme nº andar: ");
@@ -103,6 +123,8 @@ public class Principal {
     // Método aptsOcup(), Conta a quantidade de apartamentos ocupados em cada andar
     public static void aptsOcup(){
         
+        System.out.println("======================= OCUPADOS =======================");
+        
         int aptsOcup = 0;// variável contadora
         
         // varredura pelos apartamentos de cada andar
@@ -124,6 +146,8 @@ public class Principal {
     }
     // Método maiorMenor(), encontra os andares com maior e menor quantidade de moradores
     public static void maiorMenor(){
+        
+        System.out.println("==================== MAIS E MENOS OCUPADOS ====================");
         
         int maior = 0; // conta o andar com o maior número de moradores
         int menor = 999; // conta o andar com o menor número de moradores
@@ -151,24 +175,91 @@ public class Principal {
         System.out.println("Andar com a menor quantidade de moradores: " + andarMenor);
         
     }
-    
+    // Método idadeInferior(), conta moradores com idades inferiores a 30 anos
     public static void idadeInferior(){
         
-        int moradores = 0;
+        System.out.println("===================== IDADES INFERIORES =====================");
         
+        int moradores = 0;// variável contadora
+        
+        // varredura pelos os andares
         for(Andar andar : p.getAndares()){
             for(Apartamento apartamento : andar.getApartamentos()){
-                
+                // se morador for diferente de null e sua idade for < 30
                 if((apartamento.getMorador() != null) &&
                         (apartamento.getMorador().getIdade() < 30)){
                     
+                    moradores++; // incrementa 1 à variável contadora
+                    
+                }
+            }
+        }
+        // mostra na tela
+        System.out.println("Quantidade de moradores com idade inferior a 30 anos: " + moradores);
+        
+    }
+    // Método porcentagem(), calcula a porcentagem de moradores por andar
+    public static void porcentagem(){
+        
+        System.out.println("======================= PORCENTAGEM =======================");
+        
+        double moradores = 0; // contador de moradores por andar
+        
+        // faz uma varredura pelo prédio
+        for(Andar andar : p.getAndares()){
+            for(Apartamento apartamento : andar.getApartamentos()){
+                if(apartamento.getMorador() != null){
                     moradores++;
+                    
+                }
+            }
+            // mostra na tela
+            System.out.printf("Andar %d: %.1f%%%n", andar.getAndar(), ((moradores/10)*100));
+            moradores = 0;// renicia a variável
+        }     
+    }
+    // Método sexoFem(), conta a quantidade de mulheres no prédio
+    public static void sexoFem(){
+        
+        System.out.println("======================= SEXO FEMININO =======================");
+        
+        int fem = 0; // variável contadora
+        // varredura pelo prédio
+        for(Andar andar : p.getAndares()){
+            for(Apartamento apartamento : andar.getApartamentos()){
+                // se o morador for do sexo femino,
+                if(apartamento.getMorador() != null && apartamento.getMorador().getSexo() == 'F'){
+                    fem++; // incrementa 1 a fem
+                }
+            }
+        }
+        // mostra na tela
+        System.out.println("Quantidade de moradores do sexo feminino: " + fem);
+    }
+    // Método pesquisarNome(), pesquisa moradores pelo nome
+    public static void pesquisarNome(){
+        
+        System.out.println("======================= PESQUISAR =======================");
+        
+        boolean encontrado = false;
+        
+        System.out.println("Nome: ");// pede o nome
+        String nome = in.nextLine().toUpperCase();
+        System.out.println();
+        
+        // varredura pelo prédio atrás do nome informado
+        for(Andar andar : p.getAndares()){
+            for(Apartamento apartamento : andar.getApartamentos()){
+                if(apartamento.getMorador() != null && apartamento.getMorador().getNome().equals(nome)){
+                    encontrado = true;
+                    System.out.println(apartamento.getMorador());
                     
                 }
             }
         }
         
-        System.out.println("Quantidade de moradores com idade inferior a 30 anos: " + moradores);
-        
+        if(!encontrado){
+            System.out.println("MORADOR INEXISTENTE!\n");
+        }
     }
 }
